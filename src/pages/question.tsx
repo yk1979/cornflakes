59b0f38bd2d6601043a38ac9fdff614b572b6c73
yk1979 <yukiirisawa1979@gmail.com>
2021-01-30 16:06:36 +0900

    destructing questions

--- src/pages/question.tsx
+++ src/pages/question.tsx
@@ -1,69 +1,51 @@
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import Button from "../components/Button";
 import Layout from "../components/Layout";
 import Table from "../components/Table";
 import TableItem from "../components/Table/TableItem";
 
-// mock data
-const mock = [
-  {
-    category: "HTML/CSS",
-    questions: [
-      "規約に従ってデザインデータを正しく再現できる",
-      "コーディング規約を作ることができる",
-      "Lighthouseの結果を見て適切にマークアップの問題点を改善できる",
-      "DOM王（※必要最低限のDOMでデザイン通りのマークアップができる人）になれる",
-      "レスポンシブなマークアップができる",
-      "クロスブラウザ対応ができる",
-    ],
-  },
-  {
-    category: "JS/TS",
-    questions: [
-      "Object や Array を活用してデータを表現したり操作できる",
-      "String.prototype.includes Array.prototype.reduce RegExp.prototype.test などの基本的なメソッドを使える",
-      "tc39 proposal で ディスカッションされている実験的なシンタックスがあることをしっている",
-      "document.querySelector や document.createElement など DOM に関する関数を使える",
-      "devtools を活用して 時間のかかっている関数 や 呼び出し頻度の高い関数を見つけることができる",
-      ".tsconfigの内容を理解し、自分で書くことができる",
-      "strict modeでTypeScriptが書ける",
-      "TypeScript で 外部ライブラリに型を付けることができる",
-      "外部ライブラリを用いたTypeScriptが書ける（ex. ReactやStyled Components等を組み合わせて使えるか）",
-      "service workerを使ってオフラインでもキャッシュを用いてページをユーザーに見せることができる",
-    ],
-  },
-];
+type Props = {
+  // TODO 仮置きなのでもうちょっと考える
+  data: {
+    category: string;
+    text: string[];
+  }[];
+};
 
-// TODO fix
-const data = mock.flatMap((data) =>
-  data.questions.map((d) => ({
-    category: data.category,
-    text: d,
-  }))
-);
-
-const QuestionPage: NextPage = () => {
+const QuestionPage: NextPage<Props> = ({ data }) => {
   const router = useRouter();
+  const [dataIndex, setDataIndex] = useState(0);
+  const currentData = data[dataIndex];
+
+  // TODO ブラウザの戻る/進むイベントを検知していい感じにしたい
+  const handleChangeQuestions = (n: 1 | -1) => {
+    if (n > 0) {
+      window.history.pushState(null, String(dataIndex + n), "/question");
+    } else {
+      window.history.back();
+    }
+    setDataIndex(dataIndex + n);
+  };
+
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log("p-chan");
     router.push("/user/p-chan");
   };
+
   return (
     <Layout>
       <h1 className="text-2xl font-semibold">スキルチェック</h1>
-      {/* TODO fix */}
       <form onSubmit={(e) => handleSubmit(e)}>
         <Table headers={["category", "description", "check"]}>
-          {data.map((d, i) => (
+          {currentData.text.map((item, i) => (
             <tr key={i}>
               <TableItem className="px-6 py-4 whitespace-nowrap">
-                {d.category}
+                {currentData.category}
               </TableItem>
               <TableItem className="px-6 py-4 whitespace-nowrap">
-                {d.text}
+                {item}
               </TableItem>
               <TableItem className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 {/* TODO スタイル調整 */}
@@ -101,3 +83,41 @@ const QuestionPage: NextPage = () => {
 };
 
 export default QuestionPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  // mock data
+  const mock = [
+    {
+      category: "HTML/CSS",
+      text: [
+        "規約に従ってデザインデータを正しく再現できる",
+        "コーディング規約を作ることができる",
+        "Lighthouseの結果を見て適切にマークアップの問題点を改善できる",
+        "DOM王（※必要最低限のDOMでデザイン通りのマークアップができる人）になれる",
+        "レスポンシブなマークアップができる",
+        "クロスブラウザ対応ができる",
+      ],
+    },
+    {
+      category: "JS/TS",
+      text: [
+        "Object や Array を活用してデータを表現したり操作できる",
+        "String.prototype.includes Array.prototype.reduce RegExp.prototype.test などの基本的なメソッドを使える",
+        "tc39 proposal で ディスカッションされている実験的なシンタックスがあることをしっている",
+        "document.querySelector や document.createElement など DOM に関する関数を使える",
+        "devtools を活用して 時間のかかっている関数 や 呼び出し頻度の高い関数を見つけることができる",
+        ".tsconfigの内容を理解し、自分で書くことができる",
+        "strict modeでTypeScriptが書ける",
+        "TypeScript で 外部ライブラリに型を付けることができる",
+        "外部ライブラリを用いたTypeScriptが書ける（ex. ReactやStyled Components等を組み合わせて使えるか）",
+        "service workerを使ってオフラインでもキャッシュを用いてページをユーザーに見せることができる",
+      ],
+    },
+  ];
+
+  return {
+    props: {
+      data: mock,
+    },
+  };
+};
