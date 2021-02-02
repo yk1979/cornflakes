963aadb8037b446b1efebc2f8c3443c55df48050
yk1979 <yukiirisawa1979@gmail.com>
2021-02-03 01:20:55 +0900

    add header link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => (
   <header className="p-4 bg-primary">
-    <span className="text-xl font-semibold text-white">cornflakes</span>
+    <Link href="/">
+      <a className="text-xl font-semibold text-white">cornflakes</a>
+    </Link>
   </header>
 );
 
