c435f9f19bc85deb23fd8fcc670df3b33f2938d9
yk1979 <yukiirisawa1979@gmail.com>
2021-03-22 22:39:01 +0900

    delete unused configs

--- next.config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-module.exports = {
-  env: {
-    PROJECT_ENV: process.env.PROJECT_ENV,
-    CONTENTFUL_SPACE_ID: "dojktgqqvjkl",
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
-  },
-};
